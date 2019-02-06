5e7030db48ea114697c8ad982677bf2350763766
Michal <michalk.rejowski@gmail.com>
2019-02-06 21:55:34 +0100

    first commit

--- Beginning/Ships/src/Battleground.java
+++ Beginning/Ships/src/Battleground.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Battleground {
 
     static int[][] playground;
     static int row;
     static int col;
-    static int guess;
-    static int guess_row;
-    static int guess_col;
+    static int yourGuess;
+    static int guessRow;
+    static int guessCol;
+    static int compRow;
+    static int compCol;
+
     Scanner write = new Scanner(System.in);
 
 
-
-
-
-    public int[][] SetMap(int x, int y){                        // metoda tworząca mapę
+    public int[][] setMap(int x, int y) {                        // metoda tworząca mapę
 
         int[][] battleground = new int[x][y];
-        for (int i=0; i<battleground.length;i++){
-            for (int j=0; j<battleground[i].length;j++) {
+        for (int i = 0; i < battleground.length; i++) {
+            for (int j = 0; j < battleground[i].length; j++) {
                 if (j == y - 1) {
                     System.out.println(battleground[i][j]);
-                }
-                else {
+                } else {
                     System.out.print(battleground[i][j]);
                 }
             }
@@ -31,25 +31,25 @@ public class Battleground {
         return playground;
     }
 
-    public void SetShip(int x, int y){                          //metoda ustawiająca statek
+    public void setShip(int x, int y) {                          //metoda ustawiająca statek
 
-       playground[x-1][y-1]=1;
+        playground[x - 1][y - 1] = 1;
 
     }
 
-    public int Guess(int x,int y){                              //metoda informująca o strzale
-        if (playground[x-1][y-1] == 1){
-            guess = 1;
+    public int guess(int x, int y) {                              //metoda informująca o strzale
+        if (playground[x - 1][y - 1] == 1) {
+            yourGuess = 1;
             System.out.println("Trafiony");
-        }
-        else {
-            guess = 0;
+        } else {
+            yourGuess = 0;
             System.out.println("Pudło");
         }
-        return guess;
+        return yourGuess;
     }
 
-    public void CheckMap (int[][] tab){                         //odświeża mape
+    /*
+    public void checkMap (int[][] tab){                         //odświeża mape
 
         for (int i=0; i<tab.length;i++){
             for (int j=0; j<tab[i].length;j++) {
@@ -63,17 +63,29 @@ public class Battleground {
             }
         }
     }
-    public void Shot(int x, int y){
+    */
+    public void shot() {
         System.out.println("który wiersz?");
-        guess_row = write.nextInt();                   //zgadywanie wiersza położenia statku
+        guessRow = write.nextInt();                           //zgadywanie wiersza położenia statku
         System.out.println("która kolumna");
-        guess_col = write.nextInt();                   //zgadywanie kolumny położenia statku
+        guessCol = write.nextInt();                           //zgadywanie kolumny położenia statku
+    }
+
+    public void shotComp() {
+        Random compTurn = new Random();
+        compRow = compTurn.nextInt(row) + 1;
+        compCol = compTurn.nextInt(col) + 1;
+
+
     }
 
 
     public static void main(String[] args) {
-        Battleground player = new Battleground();              // tworzenie obiektu mapy
+        Battleground player = new Battleground();              // tworzenie obiektu gracza
+        Battleground comp = new Battleground();                // tworzenie obiektu komputera
+
         Scanner write = new Scanner(System.in);                // tworzenie obiektu opcji wejścia
+
 
         System.out.println("Podaj liczbę wierszy");
         row = write.nextInt();                                 //podajesz liczbę wierszy
@@ -81,20 +93,26 @@ public class Battleground {
         col = write.nextInt();                                 //podajesz liczbę kolumn
 
 
-        player.SetMap(row,col);                                //wywoływanie metody tworzącej mapę
+        player.setMap(row, col);                                //tworzenie mapy
 
-        System.out.println("Podaj wiersz gdzie chcesz ustawić statek");
-        int cord_x = write.nextInt();                          //podajesz który wiersz
+        /*System.out.println("Podaj wiersz gdzie chcesz ustawić statek");
+        int cord_x = write.nextInt();                                   //podajesz który wiersz
         System.out.println("Podaj kolumne gdzie chcesz ustawić statek");
-        int cord_y = write.nextInt();                          //podajesz która kolumna
+        int cord_y = write.nextInt();                                   //podajesz która kolumna
 
-        player.SetShip(cord_x, cord_y);                        //wywoływanie metody ustawiającej statek
-        player.CheckMap(playground);                           //sprawdzanie stanu mapy
+        player.setShip(cord_x, cord_y);                        //wywoływanie metody ustawiającej statek
+        */
+
+        comp.shotComp();
+        comp.setShip(compRow,compCol);
+        //System.out.println(compRow + " " + compCol);         //wyświetlenie strzału komputera
+        //player.checkMap(playground);                         //odświerzenie mapy
+
         System.out.println("strzelaj!");
 
         do {
-            player.Shot(guess_row,guess_col);
-            player.Guess(guess_row, guess_col);                 //wywoływanie metody sprawdzającej czy próba zgadnięcia była skuteczna
-        }while (guess == 0);
+            player.shot();
+            player.guess(guessRow, guessCol);                 //wywoływanie metody sprawdzającej czy próba zgadnięcia była skuteczna
+        } while (yourGuess == 0);
     }
 }
